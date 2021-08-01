3bcc6df00f72708fcc52f009d74112f6437bcbaa
Leonardo Russo <info@lrusso.com>
2021-08-01 01:36:57 -0300

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,35 @@
+const filesToCache = [
+	"EndlessRunner.htm",
+	"EndlessRunner.json",
+	"EndlessRunner.png",
+	"EndlessRunnerFavIcon_16x16.png",
+	"EndlessRunnerFavIcon_192x192.png",
+	"EndlessRunnerFavIcon_512x512.png",
+	"EndlessRunnerGame.htm",
+	"EndlessRunnerGame.js",
+	"EndlessRunnerShare.png"
+];
+
+const staticCacheName = "endlessrunner-v1";
+
+self.addEventListener("install", event => {
+	event.waitUntil(
+		caches.open(staticCacheName)
+		.then(cache => {
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.match(event.request)
+		.then(response => {
+			if (response) {
+				return response;
+			}
+			return fetch(event.request)
+		}).catch(error => {
+		})
+	);
+});
