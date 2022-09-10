b8ddeb75f3cd93be96b4292a1e9ae03f71a17d01
Leonardo Russo <info@lrusso.com>
2022-09-09 23:35:53 -0300

    Add files via upload

--- EndlessRunnerGame.js
+++ EndlessRunnerGame.js
@@ -1152,7 +1152,7 @@ EndlessRunner.Game.prototype = {
 				{
 				var date = new Date();
 				date.setTime(date.getTime() + (days*24*60*60*1000));
-				expires = "; expires=" + date.toUTCString();
+				expires = "; expires=" + date.toUTCString() + "; SameSite=Lax";
 				}
 			document.cookie = name + "=" + (value || "")  + expires + "; Secure; path=/";
 			}
